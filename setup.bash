42458ef10f4a134da7f9a35e519e52c075df0e6f
Parth Santpurkar <parthsantpurkar@gmail.com>
2013-02-03 14:02:02 -0500

    changed ssh port back to 65535 and removed ifaces.py support

--- setup.bash
+++ setup.bash
@@ -7,12 +7,12 @@ sudo apt-get update
 sudo apt-get -y install python-pip gcc python-dev
 sudo pip install netifaces
 sudo wget https://raw.github.com/parthsantpurkar/honeypot-setup-script/master/scripts/iface-choice.py -O /tmp/iface-choice.py
-python /tmp/iface-choice.py
-iface=$(<~/.honey_iface)
+#python /tmp/iface-choice.py
+#iface=$(<~/.honey_iface)
 
 
-# Move SSH server from Port 22 to Port 9000
-sudo sed -i 's:Port 22:Port 9000:g' /etc/ssh/sshd_config
+# Move SSH server from Port 22 to Port 65535
+sudo sed -i 's:Port 22:Port 65535:g' /etc/ssh/sshd_config
 sudo service ssh reload
 
 
